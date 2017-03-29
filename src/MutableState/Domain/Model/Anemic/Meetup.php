78df9d7ced006bcdebb85f6bfc3635d642b510fe
Matthias Noback <matthiasnoback@gmail.com>
2017-03-29 10:00:27 +0200

    Add anemic domain model example

---
+++ src/MutableState/Domain/Model/Anemic/Meetup.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace MutableState\Domain\Model\Anemic;
+
+final class Meetup
+{
+    private $id;
+    private $scheduledDate;
+
+    public function __construct(string $scheduledDate)
+    {
+        $this->scheduledDate = $scheduledDate;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setScheduledDate(string $newDate): void
+    {
+        $this->scheduledDate = $newDate;
+    }
+
+    public function getScheduledDate(): string
+    {
+        return $this->scheduledDate;
+    }
+}
